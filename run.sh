70f6d7de18d9165badf36d37893cb09c05194b63
Masroor Ahmad <99042825+masroor-ahmed@users.noreply.github.com>
2024-06-30 13:03:11 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# Start Gophish with the adjusted configuration
+# Adjust the config.json for dynamic port binding
+if [ -n "$PORT" ]; then
+  jq --arg port "$PORT" '.phish_server.listen_url = "0.0.0.0:" + $port' config.json > config.json.tmp && mv config.json.tmp config.json
+fi
+
+# Start Gophish
 ./gophish -config config.json
