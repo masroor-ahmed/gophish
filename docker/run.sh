7227da2d9efe4f1608ba29e1ff7d444fd90bf1b2
Masroor Ahmad <99042825+masroor-ahmed@users.noreply.github.com>
2024-06-30 13:05:14 +0500

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,88 +1,42 @@
-#!/bin/bash
+# Minify client-side assets (JavaScript)
+FROM node:latest AS build-js
 
-# set config for admin_server
-if [ -n "${ADMIN_LISTEN_URL+set}" ] ; then
-    jq -r \
-        --arg ADMIN_LISTEN_URL "${ADMIN_LISTEN_URL}" \
-        '.admin_server.listen_url = $ADMIN_LISTEN_URL' config.json > config.json.tmp && \
-        cat config.json.tmp > config.json
-fi
-if [ -n "${ADMIN_USE_TLS+set}" ] ; then
-    jq -r \
-        --argjson ADMIN_USE_TLS "${ADMIN_USE_TLS}" \
-        '.admin_server.use_tls = $ADMIN_USE_TLS' config.json > config.json.tmp && \
-        cat config.json.tmp > config.json
-fi
-if [ -n "${ADMIN_CERT_PATH+set}" ] ; then
-    jq -r \
-        --arg ADMIN_CERT_PATH "${ADMIN_CERT_PATH}" \
-        '.admin_server.cert_path = $ADMIN_CERT_PATH' config.json > config.json.tmp && \
-        cat config.json.tmp > config.json
-fi
-if [ -n "${ADMIN_KEY_PATH+set}" ] ; then
-    jq -r \
-        --arg ADMIN_KEY_PATH "${ADMIN_KEY_PATH}" \
-        '.admin_server.key_path = $ADMIN_KEY_PATH' config.json > config.json.tmp && \
-        cat config.json.tmp > config.json
-fi
-if [ -n "${ADMIN_TRUSTED_ORIGINS+set}" ] ; then
-    jq -r \
-        --arg ADMIN_TRUSTED_ORIGINS "${ADMIN_TRUSTED_ORIGINS}" \
-        '.admin_server.trusted_origins = ($ADMIN_TRUSTED_ORIGINS|split(","))' config.json > config.json.tmp && \
-        cat config.json.tmp > config.json
-fi
+RUN npm install gulp gulp-cli -g
 
-# set config for phish_server
-if [ -n "${PHISH_LISTEN_URL+set}" ] ; then
-    jq -r \
-        --arg PHISH_LISTEN_URL "${PHISH_LISTEN_URL}" \
-        '.phish_server.listen_url = $PHISH_LISTEN_URL' config.json > config.json.tmp && \
-        cat config.json.tmp > config.json
-fi
-if [ -n "${PHISH_USE_TLS+set}" ] ; then
-    jq -r \
-        --argjson PHISH_USE_TLS "${PHISH_USE_TLS}" \
-        '.phish_server.use_tls = $PHISH_USE_TLS' config.json > config.json.tmp && \
-        cat config.json.tmp > config.json
-fi
-if [ -n "${PHISH_CERT_PATH+set}" ] ; then
-    jq -r \
-        --arg PHISH_CERT_PATH "${PHISH_CERT_PATH}" \
-        '.phish_server.cert_path = $PHISH_CERT_PATH' config.json > config.json.tmp && \
-        cat config.json.tmp > config.json
-fi
-if [ -n "${PHISH_KEY_PATH+set}" ] ; then
-    jq -r \
-        --arg PHISH_KEY_PATH "${PHISH_KEY_PATH}" \
-        '.phish_server.key_path = $PHISH_KEY_PATH' config.json > config.json.tmp && \
-        cat config.json.tmp > config.json
-fi
+WORKDIR /build
+COPY . .
+RUN npm install --only=dev
+RUN gulp
 
-# set contact_address
-if [ -n "${CONTACT_ADDRESS+set}" ] ; then
-    jq -r \
-        --arg CONTACT_ADDRESS "${CONTACT_ADDRESS}" \
-        '.contact_address = $CONTACT_ADDRESS' config.json > config.json.tmp && \
-        cat config.json.tmp > config.json
-fi
+# Build Golang binary
+FROM golang:1.15.2 AS build-golang
 
-# db_name has to be changed to mysql for mysql connection to work
-if [ -n "${DB_NAME+set}" ] ; then
-    jq -r \
-        --arg DB_NAME "${DB_NAME}" \
-        '.db_name = $DB_NAME' config.json > config.json.tmp && \
-        cat config.json.tmp > config.json
-fi
+WORKDIR /go/src/github.com/gophish/gophish
+COPY . .
+RUN go get -v && go build -v
 
-if [ -n "${DB_FILE_PATH+set}" ] ; then
-    jq -r \
-        --arg DB_FILE_PATH "${DB_FILE_PATH}" \
-        '.db_path = $DB_FILE_PATH' config.json > config.json.tmp && \
-        cat config.json.tmp > config.json
-fi
+# Runtime container
+FROM debian:stable-slim
 
-echo "Runtime configuration: "
-cat config.json
+RUN useradd -m -d /opt/gophish -s /bin/bash app
 
-# start gophish
-./gophish
+RUN apt-get update && \
+    apt-get install --no-install-recommends -y jq libcap2-bin ca-certificates && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+WORKDIR /opt/gophish
+COPY --from=build-golang /go/src/github.com/gophish/gophish/ ./
+COPY --from=build-js /build/static/js/dist/ ./static/js/dist/
+COPY --from=build-js /build/static/css/dist/ ./static/css/dist/
+COPY ./config.json ./docker/
+COPY ./run.sh ./docker/
+
+RUN chown -R app:app /opt/gophish
+RUN chmod +x ./docker/run.sh
+
+USER app
+
+EXPOSE 3333 8080 8443 80
+
+CMD ["./docker/run.sh"]
